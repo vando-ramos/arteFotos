00a0a7fe070cf8e0ebc8b911abd7450ab3c6e3f5
vando ramos <vandorramos@gmail.com>
2025-06-10 22:00:34 -0300

    Create layout for image upload and image style pages

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,7 @@
+class ImagesController < ApplicationController
+  def upload    
+  end
+
+  def style    
+  end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do  
   root "home#index"
+  
   get '/login', to: 'login#new'
+
+  resources :images do
+    collection do
+      get 'upload'
+      get 'style'
+    end
+  end
 end
